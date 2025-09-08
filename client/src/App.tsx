938ddff081b93c57a36d3aa1527531ab67159ed3
Emmanuel Ogugua <emmachuka@gmail.com>
2025-09-08 01:11:08 +0100

    fix: Add QueryClientProvider to resolve React Query error
    - Added QueryClient and QueryClientProvider imports to App.tsx
    - Wrapped Router with QueryClientProvider to enable useMutation in contact form
    - Fixes runtime error: 'No QueryClient set, use QueryClientProvider to set one'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Router } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "@/pages/home";
 import { About } from "@/pages/about";
 import Learn from "@/pages/learn";
@@ -8,18 +9,22 @@ import { Consulting } from "@/pages/consulting";
 import { Training } from "@/pages/training";
 import NotFound from "@/pages/not-found";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/learn" component={Learn} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/development" component={Development} />
-      <Route path="/consulting" component={Consulting} />
-      <Route path="/training" component={Training} />
-      <Route path="*" component={NotFound} />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/learn" component={Learn} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/development" component={Development} />
+        <Route path="/consulting" component={Consulting} />
+        <Route path="/training" component={Training} />
+        <Route path="*" component={NotFound} />
+      </Router>
+    </QueryClientProvider>
   );
 }
 
